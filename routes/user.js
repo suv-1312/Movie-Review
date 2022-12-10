8fbd53d9f70369d2f360ac9e5083a35f531ec43c
suv-1312 <96910024+suv-1312@users.noreply.github.com>
2022-12-11 01:49:06 +0530

    Add files via upload

---
+++ routes/user.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const {
+	create,
+	verifyEmail,
+	resendEmailVerificationToken,
+	forgetPassword
+
+} = require('../controllers/user');
+
+const {userValidator,validate} = require('../middlewares/validator');
+
+const router = express.Router();
+
+router.post('/create',userValidator,validate,create);
+router.post('/verify-email',verifyEmail);
+router.post('/resend-email-verification-token',resendEmailVerificationToken);
+router.post('/forget-password',forgetPassword);
+
+
+
+module.exports = router;
